5627e35167940e636d510679664ededfd111e624
Leniza Mendes <lenizaomnd@gmail.com>
2026-02-11 21:46:17 -0400

    complet

--- app/controller/Sale.php
+++ app/controller/Sale.php
@@ -10,7 +10,8 @@ class Sale extends Base
     public function cadastro($request, $response)
     {
         $dadosTemplate = [
-            'titulo' => 'Página inicial'
+            'titulo' => 'Página inicial',
+            'acao' => 'c'
         ];
         return $this->getTwig()
             ->render($response, $this->setView('sale'), $dadosTemplate)
@@ -29,19 +30,33 @@ class Sale extends Base
     }
     public function insert($request, $response)
     {
+        #captura os dados do formulário
         $form = $request->getParsedBody();
+        #Captura o id do produto
         $id_produto = $form['pesquisa'];
+        #Verificar se o id do produto esta vasio ou nulo
         if (empty($id_produto) or is_null($id_produto)) {
-            return $this->SendJson($response, ['status' => false, 'msg' => 'Restrição O Id do Produto é obrigatório', 'id' => 0], 403);
+            return $this->SendJson($response, [
+                'status' => false,
+                'msg' => 'Restrição: O ID do produto é obrigatório!',
+                'id' => 0
+            ], 403);
         }
+        #seleciona o id do cliente CONSUMIDOR FINAL para inserir a venda
         $customer = SelectQuery::select('id')
             ->from('customer')
             ->order('id', 'asc')
             ->limit(1)
             ->fetch();
+        #Verificar se o cliente não foi encontrado
         if (!$customer) {
-            return $this->SendJson($response, ['status' => false, 'msg' => 'RESTIÇÃO: Nenhum cliente Encontrado!', 'id' => 0], 403);
+            return $this->SendJson($response, [
+                'status' => false,
+                'msg' => 'Restrição: Nenhum cliente encontrado!',
+                'id' => 0
+            ], 403);
         }
+        #seleciona o id do cliente CONSUMIDOR FINAL para inserir a venda
         $id_customer = $customer['id'];
         $FieldAndValue = [
             'id_cliente' => $id_customer,
@@ -52,22 +67,46 @@ class Sale extends Base
             'observacao' => ''
         ];
         try {
+            #Tenta inserir a venda no banco de dados e captura o resultado da inserção
             $IsInserted = InsertQuery::table('sale')->save($FieldAndValue);
+            #Verificar se a inserção falhou
             if (!$IsInserted) {
-                return $this->SendJson($response, ['status' => false, 'msg' => 'Restrição: ao cadastrar venda', 'id' => 0], 403);
+                return $this->SendJson(
+                    $response,
+                    [
+                        'status' => false,
+                        'msg' => 'Restrição: Falha ao inserir a venda!',
+                        'id' => 0
+                    ],
+                    403
+                );
             }
+            #Seleciona o id da venda inserida mais recente para retornar na resposta
             $sale = SelectQuery::select('id')
                 ->from('sale')
                 ->order('id', 'desc')
                 ->limit(1)
                 ->fetch();
+            #Verificar se a venda não foi encontrada
             if (!$sale) {
-                return $this->SendJson($response, ['status' => false, 'msg' => 'Restrição: Nenhuma venda Encontrada!', 'id' => 0], 403);
+                return $this->SendJson($response, [
+                    'status' => false,
+                    'msg' => 'Restrição: Nenhuma venda encontrada!',
+                    'id' => 0
+                ], 403);
             }
-            $id_sale = $sale['id'];
-            return $this->SendJson($response, ['status' => true, 'msg' => 'Venda cadastrada com sucesso!', 'id' => $id_sale], 201);
+            $id_sale = $sale["id"];
+            return $this->SendJson($response, [
+                'status' => true,
+                'msg' => 'Venda inserida com sucesso!',
+                'id' => $id_sale
+            ], 201);
         } catch (\Exception $e) {
-            return $this->SendJson($response, ['status' => false, 'msg' => 'Restrição: ' . $e->getMessage(), 'id' => 0], 500);
+            return $this->SendJson($response, [
+                'status' => false,
+                'msg' => 'Restrição: ' . $e->getMessage(),
+                'id' => 0
+            ], 500);
         }
     }
 }
